8ba814a00afba6f562e9bbbf129db27cebc1170d
Naiko-sei <55356419+Webekk@users.noreply.github.com>
2021-11-03 19:29:36 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,6 +23,7 @@ public class Main {
         System.out.println("((false && false)&&(true && true))-> "+a6);
     }
 }
+//zadanie 1 
 /*
 System.out.println("Program przyjmuje od uzytkownika 4 inty i pisze ze sa rowne jesli sa rowne a jesli nie to niespodzianka");
         Scanner in = new Scanner(System.in);
@@ -41,3 +42,24 @@ System.out.println("Program przyjmuje od uzytkownika 4 inty i pisze ze sa rowne 
             System.out.println("Slabo");
         }
  */
+//zadanie 2 
+/* 
+     System.out.println("Zadanie stricte wyniki :)");
+        Scanner in = new Scanner(System.in);
+        // najpierw zmienne
+        int a1 =  (101 + 0) / 3;
+        double a2 = 3.0e-6 * 10000000.1;
+        boolean a3 = true && true;
+        boolean a4 = false && true;
+        boolean a5 = (false && false)||(true && true);
+        boolean a6 = (false && false)&&(true && true);
+    // wyswietlenie zmiennych 
+        System.out.println("(101 + 0)/3)-> "+a1);
+        System.out.println("(3.0e-6 * 10000000.1)-> "+a2);
+        System.out.println("((true & true))-> "+a3);
+        System.out.println("((false && true))-> "+a4);
+        System.out.println("((false && false)||(true && true))-> "+a5);
+        System.out.println("((false && false)&&(true && true))-> "+a6);
+    }
+}
+*/
